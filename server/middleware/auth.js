775f930ea72b339638ec17c2be624873a4ff6aa5
Varun Yadav <varunyadav191101@gmail.com>
2025-06-24 12:45:57 +0530

    Initial commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js"
+
+// Middleware to protect routes
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = req.headers.token;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decoded.userId).select("-password");
+        if (!user) return res.json({ success: false, message: "User Not Found" });
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// controller to check if user is authenticated
+export const checkAuth = (req, res) => {
+    res.json({ success: true, user: req.user })
+}
