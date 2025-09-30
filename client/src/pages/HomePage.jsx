456944148ed426704e44e77a99c4f1176cb8f878
Varun Yadav <varunyadav191101@gmail.com>
2025-09-30 12:26:08 +0530

    Fix casing issue for RightSidebar import

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
-import RightSidebar from '../components/RightSideBar'
+import RightSidebar from '../components/RightSidebar'
 import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
